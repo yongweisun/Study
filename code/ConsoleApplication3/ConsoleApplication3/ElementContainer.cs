8473f0f57ad8e1d25912c7e795966642d15b2e19
syw <mooniscrazy@hotmail.com>
2016-12-22 23:14:11 +0800

    ElementContainer

---
+++ code/ConsoleApplication3/ConsoleApplication3/ElementContainer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3 {
+    public class ElementContainer<T> : IEnumerable<T> {
+        private List<T> innerList = new List<T>();
+        public IEnumerator<T> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+        public void Add(T element) {
+            throw new NotImplementedException();
+        }
+
+        public class ElementEnumerator<T> : IEnumerator<T> {
+            public T Current {
+                get {
+                    throw new NotImplementedException();
+                }
+            }
+
+            object IEnumerator.Current {
+                get {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public void Dispose() {
+                //这个方法不用写
+            }
+
+            public bool MoveNext() {
+                throw new NotImplementedException();
+            }
+
+            public void Reset() {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- code/ConsoleApplication3/ConsoleApplication3/Program.cs
+++ code/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -1,65 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Collections;
-class shortContainer : IEnumerable<short>
-{
-    private short[] shorts = new short[] { 8,9,0};
-    public IEnumerator GetEnumerator()
-    {
-        return new ShortContainerEnumerator(this);
-    }
+using ConsoleApplication3;
 
-    IEnumerator<short> IEnumerable<short>.GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-
-    public class ShortContainerEnumerator : IEnumerator<short>
-    {
-        public ShortContainerEnumerator(shortContainer Container)
-        {
-            myContainer = Container; 
-        }
-        shortContainer myContainer;
-        int position = -1;
-        public bool MoveNext()
-        {
-            position++;
-            return (position<myContainer.shorts.Length);
-        }
-        public short Current
-        {
-            get
-            {
-                return myContainer.shorts[position];
-            }
-        }
-
-        object IEnumerator.Current
-        {
-            get
-            {
-                return  Current; 
-            }
-        }
-
-        public void Reset()
-        {
-            position = -1;
-        }
-
-        public void Dispose()
-        {
-        }
-    }
-}
 class program
 {
     static void Main()
     {
-        shortContainer myShortContainer = new shortContainer();
+        ElementContainer<short> myShortContainer = new ElementContainer<short>();
+        myShortContainer.Add(3);
+        myShortContainer.Add(5);
+        myShortContainer.Add(1);
         foreach (short s in myShortContainer)
         {
             Console.WriteLine(s);

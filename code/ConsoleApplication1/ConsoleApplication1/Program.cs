3721b8f80ddece5d51912f3e4bd54955083770c8
Cici9 <2370069507@qq.com>
2016-12-22 19:37:31 +0800

    泛型

---
+++ code/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+class shortContainer : IEnumerable<short>
+{
+    private short[] ints = new short[] { 8, 9, 0 };
+    public IEnumerator GetEnumerator()
+    {
+        return new ShortContainerEnumerator(this);
+    }
+
+    IEnumerator<short> IEnumerable<short>.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public class ShortContainerEnumerator : IEnumerator<short>
+    {
+        public ShortContainerEnumerator(shortContainer Container)
+        {
+            myContainer = Container;
+        }
+        shortContainer myContainer;
+        int position = -1;
+        public bool MoveNext()
+        {
+            position++;
+            return (position < myContainer.ints.Length);
+        }
+        public short Current
+        {
+            get
+            {
+                return myContainer.ints[position];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
+class program
+{
+    static void Main()
+    {
+        shortContainer myShortContainer = new shortContainer();
+        foreach (short s in myShortContainer)
+        {
+            Console.WriteLine(s);
+        }
+        Console.ReadLine();
+    }
+}
